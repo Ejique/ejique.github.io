9a953ce9619872775dd0a2ed394df7ac85f4722a
Ejique <sspirin@yandex.ru>
2021-10-01 22:05:48 +0300

    git commit

--- js/code.js
+++ js/code.js
@@ -2,9 +2,33 @@
 При клике на кнопку get started появляетс модальное окно
 возможно с анимациями
 */
-let btn = document.getElementById('modal_btn');
-let modal = document.getElementById ('modal');
-let x = document.getElementById('close');
+let str = "Hello";
+let number = 42;
+let y;
+let bool = true;
+let bool2 = false;
+let obj = {
+    id: 1,
+    name: "Имя",
+    price: 2000,
+    newPrice: 1600,
+getDiscount: function () {
+    return `Ваша скидка${(this.price - this.newPrice)/(this.price / 100)}%`;
+    },
+}
+
+console.log(obj.id);
+console.log(obj.name);
+console.log(obj.price);
+console.log(obj.newPrice);
+console.log(obj.getDiscount());
+console.log(obj.y);
+
+const btn = document.getElementById('modal_btn');
+const modal = document.getElementById ('modal');
+const x = document.getElementById('close');
+const btn_submit = document.querySelector("form button");// улучшение кнопки отправить
+const inputs = document.querySelectorAll("form input") //берёт группу элементов form inputs
 
 btn.onclick = function(){
 modal.style.display = "flex" // modal получает стиль css
@@ -14,5 +38,15 @@ x.onclick = function (){
     modal.style.display = "none"; /*при клике на крест в правом верхнем углу 
     окно с введениями Имя и Телефон исчезает*/
 
-}
+};
+btn_submit.onclick = function (event){
+    console.log(event);
+    event.preventDefault(); //event имеет сокрашение до 1й буквы e
+    if(inputs[0].value == ""){
+        alert("Вы забыли ввести имя");
+    }
+    if(inputs[1].value == ""){
+        alert("Вы забыли ввести номер телефона");
+    }    
+};
 
